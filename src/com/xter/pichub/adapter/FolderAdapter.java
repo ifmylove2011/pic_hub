494962ac9e00e0bc9733ea99e4430c914bae9d52
xter <ifmylove2011@163.com>
2016-03-15 17:11:38 +0800

    try to imageview

--- src/com/xter/pichub/adapter/FolderAdapter.java
+++ src/com/xter/pichub/adapter/FolderAdapter.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import com.xter.pichub.R;
 import com.xter.pichub.element.Folder;
-import com.xter.pichub.util.ImageLoader;
-import com.xter.pichub.view.CascadeView;
+import com.xter.pichub.util.ImageLoader2;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 /**
  * Created by XTER on 2016/1/11.
@@ -22,7 +22,7 @@ public class FolderAdapter extends BaseAdapter {
 
 	LayoutInflater layoutInflater;
 	List<Folder> folders;
-	ImageLoader loader;
+	ImageLoader2 loader;
 
 	/**
 	 * 主要传入文件夹URL值
@@ -31,7 +31,7 @@ public class FolderAdapter extends BaseAdapter {
 	 * @param list    文件夹
 	 */
 	public FolderAdapter(Context context, List<Folder> list) {
-		loader = ImageLoader.build(context);
+		loader = ImageLoader2.build(context);
 		this.layoutInflater = LayoutInflater.from(context);
 		this.folders = list;
 	}
@@ -61,8 +61,10 @@ public class FolderAdapter extends BaseAdapter {
 			view = layoutInflater.inflate(R.layout.item_folder, null);
 		else
 			view = convertView;
-		CascadeView cascadeView = (CascadeView) view.findViewById(R.id.cv_folder_cover);
-		loader.bindBitmap(uris, cascadeView, 180, 180, true);
+//		CascadeView cascadeView = (CascadeView) view.findViewById(R.id.cv_folder_cover);
+		ImageView iv = (ImageView) view.findViewById(R.id.cv_folder_cover);
+//		loader.bindBitmap(uris, cascadeView, 180, 180, true);
+		loader.bindBitmap(uris[0], iv, true);
 
 		return view;
 	}
